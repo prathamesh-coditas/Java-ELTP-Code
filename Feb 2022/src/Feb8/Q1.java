d468203f66c94a6544bb38c9c13ec4306b5e7be4
prathamesh-coditas <98110316+prathamesh.chaudhari@coditas.com>
2022-02-08 15:51:33 +0530

    Feb 8 Assignment Changes

--- Feb 2022/src/Feb8/Q1.java
+++ Feb 2022/src/Feb8/Q1.java
@@ -11,14 +11,15 @@ public class Q1 {
             try {
                 System.out.println("Enter amount to Deposit");
                 acc1.deposit(sc.nextInt());
+                System.out.println("The amount you deposited::"+acc1.getBalance());
             } catch (InvalidAmountException ae) {
-                System.out.println("InvalidAmountException Raised");
+                System.out.println("InvalidAmountException::");
             }
             System.out.println("Enter amount to Withdraw");
             acc1.withdraw(sc.nextInt());
             System.out.println("Remaining Balence is::"+acc1.getBalance());
         } catch (InsufficientBalanceException e) {
-            System.out.println("InsufficientBalanceException raised");
+            System.out.println("InsufficientBalanceException");
         } catch (Exception e) {
             System.out.println("Exception " + e.getMessage());
         }
