cf8126f019a5da8f85e814fc5c40f9d5fedac8ca
prathamesh-coditas <98110316+prathamesh.chaudhari@coditas.com>
2022-02-09 16:50:17 +0530

    Feb 9 Assignment SequenceInStream add two files contains read third file

--- Feb 2022/src/Feb9/SequenceInStream.java
+++ Feb 2022/src/Feb9/SequenceInStream.java
@@ -1,18 +1,18 @@
 package Feb9;
 //read data from multiple files
+
 import java.io.*;
 
 public class SequenceInStream {
     public static void main(String args[]) throws IOException {
-
-        FileInputStream fin1=new FileInputStream("Feb 2022/src/Feb9/txt1.txt");
-        FileInputStream fin2=new FileInputStream("Feb 2022/src/Feb9/txt2.txt");
-        FileOutputStream fout=new FileOutputStream("foutput.txt");
-
-        SequenceInputStream sin=new SequenceInputStream(fin1,fin2);
+        FileInputStream fin1 = new FileInputStream("Feb 2022/src/Feb9/txt1.txt");
+        FileInputStream fin2 = new FileInputStream("Feb 2022/src/Feb9/txt2.txt");
+        FileOutputStream fout = new FileOutputStream("foutput.txt");
+        SequenceInputStream sin = new SequenceInputStream(fin1, fin2);
         int i;
-        while((i = sin.read()) != -1) {
-            System.out.print((char)i);
+        while ((i = sin.read()) != -1) {
+            fout.write(i);
+            System.out.print((char) i);
         }
         sin.close();
         fin1.close();
