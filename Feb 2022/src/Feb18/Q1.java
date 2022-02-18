d722991041b42794bfd274ce2977a87b917c960c
prathamesh-coditas <98110316+prathamesh.chaudhari@coditas.com>
2022-02-18 11:40:04 +0530

    Feb 17 Assignment

---
+++ Feb 2022/src/Feb18/Q1.java
@@ -0,0 +1,37 @@
+package Feb18;
+class Varargs_Addition {
+    public int add(Integer ...a){
+        int sum=0;
+        for (Integer i:a) {
+            sum+=i;
+        }
+        return sum;
+    }
+}
+class Addition  {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+    public int add(int a, int b , int c, int d)
+    {
+        return a+b+c+d;
+    }
+}
+public class Add {
+    public static void main(String[] args) {
+        Varargs_Addition obj= new Varargs_Addition();
+        System.out.println("Using Varargs");
+        System.out.println("Addition is ==>"+obj.add(1,3,5,7,9,11));
+        System.out.println("Addition is ==>"+obj.add(2,4,6,8,10,12));
+        System.out.println("Addition is ==>"+obj.add(3,6,9,12,15,18));
+        System.out.println("**************************************************");
+        System.out.println(obj.add(1,2));
+        System.out.println(obj.add(1,2,3));
+        System.out.println(obj.add(1,2,3,4));
+
+    }
+
+}
---
+++ Feb 2022/src/Feb18/Q2.java
@@ -0,0 +1,26 @@
+package Feb18;
+
+enum Signal {
+    RED,GREEN,YELLOW
+}
+
+public class Q2 {
+    public static void main(String[] args) {
+        Signal[] s1 = Signal.values();
+        for (Signal color:s1) {
+
+            switch (color) {
+                case RED:
+                    System.out.println("RED MEANS STOP");
+                    break;
+                case YELLOW:
+                    System.out.println("GREEN MEANS GO");
+                    break;
+                case GREEN:
+                    System.out.println("YELLOW MEANS DRIVE VERY SLOW");
+                    break;
+
+            }
+        }
+    }
+}
